478440758fff79a5731f84296d51ef52d279ba9e
Parashara <parasharar@gmail.com>
2023-10-10 16:44:25 +0800

    Finished the code for plotting the model statistics

--- utils/plot.py
+++ utils/plot.py
@@ -1,7 +1,63 @@
-'''
-TODO.
+import matplotlib.pyplot as plt
 
-1. generic function to show the train, test loss vs epochs graph
-2. given images plot it
 
-'''
+def plot_model_stats(experiment, epochs, training_losses, validation_losses, training_accuracy, validation_accuracy):
+    fig, axes = plt.subplots(2, 2, figsize=(16, 10))
+
+    # Plot data on each subplot and add labels
+    axes[0, 0].plot(epochs, training_losses, marker="o", color="red")
+    axes[0, 0].set_title(f'{experiment}: Training Loss vs Epochs')
+    axes[0, 0].set_xlabel('Epochs')
+    axes[0, 0].set_ylabel('Training Loss')
+
+    axes[0, 1].plot(epochs, training_accuracy, marker="o", color="green")
+    axes[0, 1].set_title(f'{experiment}: Training Accuracy vs Epochs')
+    axes[0, 1].set_xlabel('Epochs')
+    axes[0, 1].set_ylabel('Training Accuracy')
+
+    axes[1, 0].plot(epochs, validation_losses, marker="o", color="red")
+    axes[1, 0].set_title(f'{experiment}: Validation Loss vs Epochs')
+    axes[1, 0].set_xlabel('Epochs')
+    axes[1, 0].set_ylabel('Validation Loss')
+
+    axes[1, 1].plot(epochs, validation_accuracy, marker="o", color="green")
+    axes[1, 1].set_title(f'{experiment}: Validation Accuracy vs Epochs')
+    axes[1, 1].set_xlabel('Epochs')
+    axes[1, 1].set_ylabel('Validation Accuracy')
+
+    # Add space between subplots
+    plt.tight_layout()
+
+    # Show the plot
+    plt.show()
+
+    # close it properly
+    plt.clf()
+    plt.cla()
+    plt.close()
+
+
+def plot_model_stats(experiment, epochs, training_losses, validation_losses):
+    fig, axes = plt.subplots(1, 2, figsize=(16, 10))
+
+    # Plot data on each subplot and add labels
+    axes[0, 0].plot(epochs, training_losses, marker="o", color="red")
+    axes[0, 0].set_title(f'{experiment}: Training Loss vs Epochs')
+    axes[0, 0].set_xlabel('Epochs')
+    axes[0, 0].set_ylabel('Training Loss')
+
+    axes[0, 1].plot(epochs, validation_losses, marker="o", color="red")
+    axes[0, 1].set_title(f'{experiment}: Validation Loss vs Epochs')
+    axes[0, 1].set_xlabel('Epochs')
+    axes[0, 1].set_ylabel('Validation Loss')
+
+    # Add space between subplots
+    plt.tight_layout()
+
+    # Show the plot
+    plt.show()
+
+    # close it properly
+    plt.clf()
+    plt.cla()
+    plt.close()
