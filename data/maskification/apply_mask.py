0a1244c5f126d27cb485e1ca72f63f7d2b23c2f0
Parashara <parasharar@gmail.com>
2023-10-12 13:13:13 +0800

    made minor changes to the apply_mask file

--- data/maskification/apply_mask.py
+++ data/maskification/apply_mask.py
@@ -48,7 +48,7 @@ class Maskify:
             data_file_path = os.path.join(self.data_path, mask_file_class, mask_file_name)
 
             #Find the corresponding masked data image path to which we will be saving the image to
-            masked_data_file_path = os.path.join(self.masked_data_path, mask_file_class, mask_file_name)
+            masked_data_file_path = os.path.join(self.masked_data_path, mask_file_class, f"masked_{mask_file_name}")
 
             # Check if the corresponding data image exists
             if os.path.exists(data_file_path):
@@ -66,7 +66,7 @@ class Maskify:
 
                 # Save the masked image to the output directory
                 os.makedirs(os.path.dirname(masked_data_file_path), exist_ok=True)
-                masked_data_image.save(masked_data_file_path)
+                masked_data_image.save(f"{masked_data_file_path}")
             else:
                 print(f"{data_file_path} not found")
         print("Finished applying masks and saving!")
@@ -74,30 +74,39 @@ class Maskify:
 
 
 
-# if __name__ == '__main__':
-    #for cam 16 dataset
-    # data_path = os.path.abspath("../../datasets/CAM16_100cls_10mask/train/data")
-    # mask_path = os.path.abspath("../../datasets/CAM16_100cls_10mask/train/mask")
-    # masked_data_path = os.path.abspath("../../datasets/CAM16_100cls_10mask/train/masked_data")
+if __name__ == '__main__':
+    # for cam 16 dataset
+    cam_data_path = os.path.abspath("../../datasets/CAM16_100cls_10mask/train/data")
+    cam_mask_path = os.path.abspath("../../datasets/CAM16_100cls_10mask/train/mask")
+    cam_masked_data_path = os.path.abspath("../../datasets/CAM16_100cls_10mask/train/masked_data")
+    cam_maskify = Maskify(cam_data_path, cam_mask_path, cam_masked_data_path)
+    print("Camelyon done!")
 
     # for wbc1
-    # data_path = os.path.abspath("../../datasets/WBC_1/train/data")
-    # mask_path = os.path.abspath("../../datasets/WBC_1/train/mask")
-    # masked_data_path = os.path.abspath("../../datasets/WBC_1/train/masked_data")
+    wbc1_data_path = os.path.abspath("../../datasets/WBC_1/train/data")
+    wbc1_mask_path = os.path.abspath("../../datasets/WBC_1/train/mask")
+    wbc1_masked_data_path = os.path.abspath("../../datasets/WBC_1/train/masked_data")
+    wbc1_maskify = Maskify(wbc1_data_path, wbc1_mask_path, wbc1_masked_data_path)
+    print("WBC1 done!")
 
-    #for wbc10
-    # data_path = os.path.abspath("../../datasets/WBC_10/train/data")
-    # mask_path = os.path.abspath("../../datasets/WBC_10/train/mask")
-    # masked_data_path = os.path.abspath("../../datasets/WBC_10/train/masked_data")
+    # for wbc10
+    wbc10_data_path = os.path.abspath("../../datasets/WBC_10/train/data")
+    wbc10_mask_path = os.path.abspath("../../datasets/WBC_10/train/mask")
+    wbc10_masked_data_path = os.path.abspath("../../datasets/WBC_10/train/masked_data")
+    wbc10_maskify = Maskify(wbc10_data_path, wbc10_mask_path, wbc10_masked_data_path)
+    print("WBC10 done!")
 
-    #for wbc50
-    # data_path = os.path.abspath("../../datasets/WBC_50/train/data")
-    # mask_path = os.path.abspath("../../datasets/WBC_50/train/mask")
-    # masked_data_path = os.path.abspath("../../datasets/WBC_50/train/masked_data")
+    # for wbc50
+    wbc50_data_path = os.path.abspath("../../datasets/WBC_50/train/data")
+    wbc50_mask_path = os.path.abspath("../../datasets/WBC_50/train/mask")
+    wbc50_masked_data_path = os.path.abspath("../../datasets/WBC_50/train/masked_data")
+    wbc50_maskify = Maskify(wbc50_data_path, wbc50_mask_path, wbc50_masked_data_path)
+    print("WBC50 done!")
 
-    #for wbc100
-    # data_path = os.path.abspath("../../datasets/WBC_100/train/data")
-    # mask_path = os.path.abspath("../../datasets/WBC_100/train/mask")
-    # masked_data_path = os.path.abspath("../../datasets/WBC_100/train/masked_data")
+    # for wbc100
+    wbc100_data_path = os.path.abspath("../../datasets/WBC_100/train/data")
+    wbc100_mask_path = os.path.abspath("../../datasets/WBC_100/train/mask")
+    wbc100_masked_data_path = os.path.abspath("../../datasets/WBC_100/train/masked_data")
+    wbc100_maskify = Maskify(wbc100_data_path, wbc100_mask_path, wbc100_masked_data_path)
+    print("WBC100 done!")
 
-    # maskify = Maskify(data_path, mask_path, masked_data_path)
